3dd1907fddb5b05e8dedf1c18d7d6f3364e50be1
Nathan6006 <nliu44139@gmail.com>
2025-11-27 23:39:20 -0500

    11/27

--- data/data_files_to_merge/Lee_unsat/temp.py
+++ data/data_files_to_merge/Lee_unsat/temp.py
@@ -1,4 +1,5 @@
 import pandas as pd
-df = pd.read_csv("formulations.csv")
-df["Dosage"] = 25
-df.to_csv("formulation.csv", index=False)
+df = pd.read_csv("individual_metadata_.csv")
+
+df.drop(columns=["MolWt"], inplace=True)
+df.to_csv("individual_metadata.csv", index=False)
--- data/data_files_to_merge/Yu_Aminoglycoside/temp.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("main_data.csv")
-df["quantified_toxicity"] = df["quantified_toxicity"] * 100
-
-df.to_csv("main_data_.csv", index=False)
