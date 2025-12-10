9b2c36a132882671cc79088f7ef7333a49883634
Nathan6006 <nliu44139@gmail.com>
2025-12-10 00:24:01 -0500

    12/10

--- data/data_files_to_merge/Lee_unsat/temp.py
+++ data/data_files_to_merge/Lee_unsat/temp.py
@@ -1,5 +1,4 @@
 import pandas as pd
-df = pd.read_csv("individual_metadata_.csv")
-
-df.drop(columns=["MolWt"], inplace=True)
-df.to_csv("individual_metadata.csv", index=False)
+df = pd.read_csv("main_data.csv")
+df.drop(columns=["quantified_delivery"], inplace=True)
+df.to_csv("main_data_.csv", index=False)
